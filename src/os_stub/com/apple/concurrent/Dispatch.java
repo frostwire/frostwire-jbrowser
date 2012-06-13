d5a407709cf6b052aa20e42f75bbc43f5314573e
aldenml <aldenml@gmail.com>
2012-06-13 14:28:30 -0400

    Missing stub files.

---
+++ src/os_stub/com/apple/concurrent/Dispatch.java
@@ -0,0 +1,14 @@
+package com.apple.concurrent;
+
+import java.util.concurrent.Executor;
+
+public class Dispatch {
+
+    public static Dispatch getInstance() {
+        return null;
+    }
+
+    public Executor getBlockingMainQueueExecutor() {
+        return null;
+    }
+}
---
+++ src/os_stub/com/apple/eawt/CocoaComponent.java
@@ -0,0 +1,15 @@
+package com.apple.eawt;
+
+import java.awt.Component;
+
+public class CocoaComponent extends Component {
+
+    private static final long serialVersionUID = 7600039181558780100L;
+
+    public long createNSViewLong() {
+        return 0;
+    }
+
+    protected void sendMessage(int id, Object obj) {
+    }
+}
